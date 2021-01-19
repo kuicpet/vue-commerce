bb625c02d6075ee15c2091ac93debd73503e06fb
Kingsley <kingsleyumujeyan@gmail.com>
2021-01-19 09:24:02 +0100

    ft--added views

---
+++ src/views/home/Products.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4" v-for="product in products" :key="product.id">
+              
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import ProductCard from "../../components/ProductCard"
+export default {
+    computed: {
+        ...mapGetters("product", ["products"]),
+    },
+    components: { ProductCard },
+    methods: {
+        ...mapActions("product", ["getProducts", "addCart", "removeCart"]),
+    },
+    mounted() {
+        this.getProducts();
+    }
+}
+</script>
+
+<style>
+
+</style>
