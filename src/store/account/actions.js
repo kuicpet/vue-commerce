f445e1c69ff0bf37ebb172da792dbbcf20ebff35
Kingsley <kingsleyumujeyan@gmail.com>
2021-01-19 05:26:01 +0100

    ft--file sturucture created

---
+++ src/store/account/actions.js
@@ -0,0 +1,19 @@
+import router from "../../router";
+import Axios from "axios";
+
+export function login({ commit }) {
+    let url = "https://randomuser.me/apa/";
+    Axios.get(url).then(function (response) {
+        let userData = {
+            displayName: response.data.results[0].name.first,
+            email: response.data.results[0].email,
+            photoUrl: response.data.results[0].picture.medium,
+            uid: response.data.results[0].login.uuid
+        }
+        commit("setUserData", userData)
+        router.push("/")
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
+}
---
+++ src/store/product/state.js
@@ -0,0 +1,7 @@
+export default function () {
+    return {
+        produsct: [],
+        product: {},
+        cart: []
+    }
+}
